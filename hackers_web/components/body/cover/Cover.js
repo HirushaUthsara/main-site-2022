74265d644330300cccb2d1601b5eeead89b804c4
AdithyaGallage <e17091@eng.pdn.ac.lk>
2021-12-26 16:28:10 +0530

    Cover background was created without the play button

---
+++ hackers_web/components/body/cover/Cover.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "./Cover.module.css";
+function Cover() {
+  return (
+    <div className={classes["cover-container"]}>
+      <h1>Hackers' Club</h1>
+      <h1>University of Peradeniya</h1>
+      <p>Imagine and Innovate... Welcome to the world of Ones & Zeros</p>
+    </div>
+  );
+}
+
+export default Cover;
