5356516ff3340e200994394ede59050eea8af8eb
LAHIRU <e17405@eng.pdn.ac.lk>
2022-01-09 19:51:31 +0530

    Navbar Added

--- hackers_web/components/body/cover/Cover.js
+++ hackers_web/components/body/cover/Cover.js
@@ -3,11 +3,11 @@ import classes from "./Cover.module.css";
 import PlayButton from "./PlayButton";
 function Cover() {
   return (
-    <div className={classes["cover-container"]}>
+    <div className={classes["cover-container"]} id="Home">
       <h1>Hackers' Club</h1>
       <h1>University of Peradeniya</h1>
       <p>Imagine and Innovate... Welcome to the world of Ones & Zeros</p>
-      <PlayButton/>
+      <PlayButton />
     </div>
   );
 }
